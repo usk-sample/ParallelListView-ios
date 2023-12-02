1400d60d91b3413f32363011add6667a2401e7a0
Yusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2023-12-02 20:42:40 +0900

    Initial Commit

---
+++ ParallelListView/ParallelListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallelListViewApp.swift
+//  ParallelListView
+//
+//  Created by Yusuke Hasegawa on 2023/12/02.
+//
+
+import SwiftUI
+
+@main
+struct ParallelListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
